22f12d6616a053c6eb1cc6470de8d4069b389257
Niharika-4335 <niharikabethapudi71@gmail.com>
2025-04-16 17:17:23 +0530

    adding files

--- src/main/java/com/example/cricket_app/dto/response/SignUpResponse.java
+++ src/main/java/com/example/cricket_app/dto/response/SignUpResponse.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 
 @Data
 public class SignUpResponse {
+    private Long id;
     private String fullName;
     private String email;
     private String role;
--- src/main/java/com/example/cricket_app/mapper/SignUpMapper.java
+++ src/main/java/com/example/cricket_app/mapper/SignUpMapper.java
@@ -3,9 +3,11 @@ package com.example.cricket_app.mapper;
 import com.example.cricket_app.dto.response.SignUpResponse;
 import com.example.cricket_app.entity.Users;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface SignUpMapper {
+    @Mapping(source = "id", target = "id")
     SignUpResponse toResponseDto(Users user);
 
 }
